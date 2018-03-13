0c52f1e75a19a7097b0c01a2dec78d937ab233cc
mila <80.mila@gmail.com>
2018-03-13 15:53:43 +0200

    add Problem 2.cs

---
+++ intro-and-basic-syntax/exercise/Problem 2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace rectangle_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = width * height;
+
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
