710f61d9922d6e5386e92ea650f55a56717f4929
Boris Mandzhukov <skipter@abv.bg>
2018-03-13 17:18:16 +0200

    Edit line 11 and line 12 on Problem 1.cs

--- intro-and-basic-syntax/exercise/Problem 1.cs
+++ intro-and-basic-syntax/exercise/Problem 1.cs
@@ -8,8 +8,8 @@ namespace _01._Debit
         {
             int first = int.Parse(Console.ReadLine());
             int second = int.Parse(Console.ReadLine());
-            int third = int.Parse(Console.ReadLine());
-            int forth = int.Parse(Console.ReadLine());
+            long third = long.Parse(Console.ReadLine());        //Edited
+            long forth = long.Parse(Console.ReadLine());        //Edited
 
             Console.WriteLine($"{first:d4} {second:d4} {third:d4} {forth:d4}");
         }
