0239e38b4f12bc7fd2bfdb4fad4c633177a6596e
Boris Mandzhukov <skipter@abv.bg>
2018-03-13 17:12:39 +0200

    Delete .cs and add again

--- intro-and-basic-syntax/exercise/Problem 3 BigPower/Problem 3 BigPower/BigPower.cs
+++ intro-and-basic-syntax/exercise/Problem 3 BigPower/Problem 3 BigPower/BigPower.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Numerics;
-namespace Problem3fromBoris
+namespace Problem_3_BigPower
 {
-    class PowNumber
+    class BigPower
     {
         static void Main()
         {
             int number = int.Parse(Console.ReadLine());
             BigInteger result = 1;
+
             for (int i = 0; i < number; i++)
             {
                 result *= number;
