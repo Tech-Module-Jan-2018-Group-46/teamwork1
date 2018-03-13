fbfd449b7ae964559ec16ebb7e1665ee5f1a88b0
Boris Mandzhukov <skipter@abv.bg>
2018-03-13 17:04:25 +0200

    Add problem3 BigPower Via Bash

---
+++ intro-and-basic-syntax/exercise/Prob3BigPowerBoris.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+namespace Problem3fromBoris
+{
+    class PowNumber
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            for (int i = 0; i < number; i++)
+            {
+                result *= number;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
