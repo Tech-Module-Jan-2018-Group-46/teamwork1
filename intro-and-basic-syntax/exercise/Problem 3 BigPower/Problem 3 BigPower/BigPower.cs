6d7fc5ccc0dbeefd821142f00c1821c99f4b4ba1
mila <80.mila@gmail.com>
2018-03-19 22:27:34 +0200

    documented Problem 3

--- intro-and-basic-syntax/exercise/Problem 3 BigPower/Problem 3 BigPower/BigPower.cs
+++ intro-and-basic-syntax/exercise/Problem 3 BigPower/Problem 3 BigPower/BigPower.cs
@@ -5,13 +5,13 @@ namespace Problem_3_BigPower
     class BigPower
     {
         static void Main()
-        {
+        {   //recieve integer from the Console
             int number = int.Parse(Console.ReadLine());
             BigInteger result = 1;
 
             for (int i = 0; i < number; i++)
             {
-                result *= number;
+                result *= number;  // multiply result to (number-1) times
             }
             Console.WriteLine(result);
         }
