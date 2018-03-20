06ec2ada57773f5e7937b5c0a22bfe3955be9c5d
Boris Mandzhukov <skipter@abv.bg>
2018-03-20 11:50:00 +0200

    Write comments to Problem 01

--- intro-and-basic-syntax/exercise/Problem 1.cs
+++ intro-and-basic-syntax/exercise/Problem 1.cs
@@ -6,11 +6,13 @@ namespace _01._Debit
     {
         static void Main(string[] args)
         {
+            // Initializing variables.
             int first = int.Parse(Console.ReadLine());
             int second = int.Parse(Console.ReadLine());
             long third = long.Parse(Console.ReadLine());        //Edited
             long forth = long.Parse(Console.ReadLine());        //Edited
 
+            //Printing result.
             Console.WriteLine($"{first:d4} {second:d4} {third:d4} {forth:d4}");
         }
     }
