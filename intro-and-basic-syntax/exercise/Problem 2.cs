d506783e931505b69fa126b70412c44d2c4e219c
Antoniyaaa <antoniya.p.ivanova@gmail.com>
2018-03-20 18:05:54 +0200

    Documented solution to Problem 2

--- intro-and-basic-syntax/exercise/Problem 2.cs
+++ intro-and-basic-syntax/exercise/Problem 2.cs
@@ -6,10 +6,14 @@ namespace rectangle_area
     {
         static void Main(string[] args)
         {
+            //Receiving width and height as double from the console
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+
+            //Calculating area
             double area = width * height;
 
+            //Writing the result on the console
             Console.WriteLine($"{area:f2}");
         }
     }
