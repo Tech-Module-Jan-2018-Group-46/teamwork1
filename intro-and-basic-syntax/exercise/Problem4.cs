177b74d42ae2e66ea8834c4673ba4f279e5194cb
Antoniyaaa <antoniya.p.ivanova@gmail.com>
2018-03-20 18:31:44 +0200

    Documented solution to Problem 4.

--- intro-and-basic-syntax/exercise/Problem4.cs
+++ intro-and-basic-syntax/exercise/Problem4.cs
@@ -10,14 +10,19 @@ namespace BeverageLabels
     {
         static void Main(string[] args)
         {
+            //Reveiving name from the console as string
             string name = Console.ReadLine();
+
+            //Reveiving ingredients from the console as double
             double volume = double.Parse(Console.ReadLine());
             double energycontent = double.Parse(Console.ReadLine());
             double sugar = double.Parse(Console.ReadLine());
 
+            //Calculating sugar and energy content
             double sper100 = (sugar / 100) * volume;
             double eper100 = (energycontent / 100) * volume;
 
+            //Writing result on the console
             Console.WriteLine($"{volume}ml {name}:");
             Console.WriteLine($"{Math.Round(eper100, 2)}kcal, {Math.Round(sper100, 2)}g sugars");
 
