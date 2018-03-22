5b5bd9ef88ef9e766171b4d4316ecc0f229323ce
Boris Mandzhukov <skipter@abv.bg>
2018-03-22 15:10:33 +0200

    Replace Problem 03 MilesToKm

---
+++ intro-and-basic-syntax/exercise/Problem 3. Miles to Kilometers/Problem 3. Miles to Kilometers/MilesToKilometers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Problem_3._Miles_to_Kilometers
+{
+    class MilesToKilometers
+    {
+        static void Main()
+        {
+            double miles = double.Parse(Console.ReadLine());
+            double result =  miles * 1.60934;
+
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
