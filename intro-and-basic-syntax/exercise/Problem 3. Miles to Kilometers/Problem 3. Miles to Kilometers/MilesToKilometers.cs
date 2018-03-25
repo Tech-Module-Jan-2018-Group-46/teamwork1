89e760045f1a3ea9cb58f09555694de49a921884
Daniel <danieltenev97gmail.com>
2018-03-25 18:43:42 +0300

    Changed variable name result to resultMiles in Problem 3.

--- intro-and-basic-syntax/exercise/Problem 3. Miles to Kilometers/Problem 3. Miles to Kilometers/MilesToKilometers.cs
+++ intro-and-basic-syntax/exercise/Problem 3. Miles to Kilometers/Problem 3. Miles to Kilometers/MilesToKilometers.cs
@@ -8,9 +8,9 @@ namespace Problem_3._Miles_to_Kilometers
         {
             //Recieve miles from the Console as double
             double miles = double.Parse(Console.ReadLine());
-            double resultmiles =  miles * 1.60934;       //Convert miles to km
+            double resultMiles =  miles * 1.60934;       //Convert miles to km
             //print result
-            Console.WriteLine($"{resultmiles:F2}");
+            Console.WriteLine($"{resultMiles:F2}");
         }
     }
 }
